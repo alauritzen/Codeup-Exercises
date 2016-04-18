fc14c003d76edc1d046379b5d96236dfa63b38a0
Alan <alaurit@gmail.com>
2016-04-18 15:08:09 -0500

    Added scalar and echos of all values including arrays

--- foreach.php
+++ foreach.php
@@ -4,16 +4,36 @@ $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 
 
 foreach ($things as $item) {
     if (is_int($item)) {
-        echo "Array item $item is an integer.";
-    if (is_float($item)) {
-        echo "Array item $item is a floating number.";
-    if (is_bool($item)) {
-        echo "Array item $item is boolean.";
-    if (is_array($item)) {
-        echo "Array item $item is an array.";
-    if (is_null($item)) {
-        echo "Array item $item is a null item.";
-    if (is_string($item)) {
-        echo "Array item $item is a string.";
+        echo "Array item $item is an integer.\n";
     }
+    if (is_float($item)) {
+        echo "Array item $item is a floating number.\n";
+    }
+    if (is_bool($item)) {
+        echo "Array item $item is boolean.\n";
+    }
+    if (is_array($item)) {
+        echo "Array item $item is an array.\n";
+    }
+    if (is_null($item)) {
+        echo "Array item $item is a null item.\n";
+    }
+    if (is_string($item)) {
+        echo "Array item $item is a string.\n";
+    }
+}
+
+foreach ($things as $item) {
+    if (is_scalar($item)) {
+        echo "Array item $item is scalar.\n";
+    }
+}
+
+foreach ($things as $item) {
+    echo "$item";
+        if (is_array($item)) {
+            foreach ($item as $part) {
+                echo "$part\n";
+            }
+        }
 }
