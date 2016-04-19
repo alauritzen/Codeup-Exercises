1ff1a4523446fd39252345a7b9846b46013e4f6f
Alan <alaurit@gmail.com>
2016-04-19 14:26:22 -0500

    added output to test functions

--- arithmetic.php
+++ arithmetic.php
@@ -20,4 +20,7 @@ function divide($a, $b)
     return $a / $b;
 }
 
-// Add code to test your functions here
+echo "Adding 5+6 = " . add(5, 6) . "\n";
+echo "Subtracting 5-6 = " . subtract(5, 6) . "\n";
+echo "Multiplying 5*6 = " . multiply(5, 6) . "\n";
+echo "Dividing 5/6 = " . divide(5, 6) . "\n";
