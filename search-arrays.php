cc262f8212d88f8e0ea467e180a2b63737ffc6da
Alan <alaurit@gmail.com>
2016-04-20 14:49:11 -0500

    corrected function to check  and return a boolean value

--- search-arrays.php
+++ search-arrays.php
@@ -4,12 +4,25 @@ $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
-$query = "Bob";
+$query = "Tina";
 
-$isFound = array_search ($query, $names);
-
-if ($isFound) {
-    echo "True\n";
-} else {
-    echo "False\n";
+function inCast($cast, $name) {
+    if (array_search($name, $cast) !== FALSE) {
+        return TRUE;
+    } else {
+        return FALSE;
+    }
 }
+echo inCast($names, $query) . PHP_EOL;
+
+
+function compareCast($cast, $comp) {
+    $count=0;
+    foreach ($cast as $castMember) {
+        if (array_search($castMember, $comp) || array_search($castMember, $comp) === 0) {
+            $count++;
+        }
+    }
+    return $count;
+}
+echo compareCast($names, $compare) . PHP_EOL;
