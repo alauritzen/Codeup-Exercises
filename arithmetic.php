998045bd3deb0d45053c9ba49a0f14d3e6ba2a72
Alan <alaurit@gmail.com>
2016-04-20 08:48:20 -0500

    added validation to check numbers and avoid div/0 in 'divide' function

--- arithmetic.php
+++ arithmetic.php
@@ -1,37 +1,68 @@
 <?php
 
-$a=100;
-$b=50;
+
+function checkNumber($a, $b)
+{
+    if (!is_numeric($a) && !is_numeric($b)) {
+        return "$a and $b must both be numbers.\n";
+    } else if (!is_numeric($a)) {
+        return "$a must be a number.\n";
+    } else if (!is_numeric($b)) {
+        return "$b must be a number.\n";
+    }
+}
 
 function add($a, $b)
 {
-    return $a + $b;
+    if (!is_numeric($a) || !is_numeric($b)) {
+        return checkNumber($a,$b);
+        return "NO!";
+    } else {
+        return $a + $b . PHP_EOL;
+    }
 }
 
 function subtract($a, $b)
 {
-    return $a - $b;
+    if (!is_numeric($a) || !is_numeric($b)) {
+        return checkNumber($a,$b);
+    } else {
+        return $a - $b . PHP_EOL;
+    }
 }
 
 function multiply($a, $b)
 {
-    return $a * $b;
+    if (!is_numeric($a) || !is_numeric($b)) {
+        return checkNumber($a,$b);
+    } else {
+        return $a * $b . PHP_EOL;
+    }
 }
 
 function divide($a, $b)
 {
-    return $a / $b;
+    if (!is_numeric($a) || !is_numeric($b)) {
+        return checkNumber($a,$b);
+    }  elseif ($b==0) {
+        return "You can't divide by $b! Are you crazy!?\n";
+    } else {
+        return $a / $b . PHP_EOL;
+    }
 }
 
 function modulus ($a, $b)
 {
-    return $a % $b;
+    if (!is_numeric($a) || !is_numeric($b)) {
+        return checkNumber($a,$b);
+    } else {
+        return $a % $b . PHP_EOL;
+    }
 }
 
-echo "Adding 5+6 = " . add(5, 6) . "\n";
-echo "Subtracting 5-6 = " . subtract(5, 6) . "\n";
-echo "Multiplying 5*6 = " . multiply(5, 6) . "\n";
-echo "Dividing 12/6 = " . divide(12, 6) . "\n";
-echo "Modulus 12/11 = " . modulus(12,11) . "\n";
 
-echo "Global variables 100, 50 passed to add function = " . add($a,$b) . "\n";
+echo add("Mogwai", "Food");
+echo subtract(5, "Joel");
+echo multiply("Paul", 6);
+echo divide(12, 0);
+echo modulus(12,"Gary");
