47ab47558573a86fa6691e619981b2a620ce2421
Alan <alaurit@gmail.com>
2016-04-21 13:46:52 -0500

    php explode/implode exercise

---
+++ list_with_commas.php
@@ -0,0 +1,18 @@
+<?php
+
+$physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+
+$physicistsArray = explode(', ', $physicistsString);
+
+// print_r($physicistsString) . PHP_EOL;
+// print_r($physicistsArray) . PHP_EOL;
+
+
+// echo "Some of the most famous fictional theoretical physicists are {$physicistsString}.\n";
+
+function humanizedList($array) {
+    $lastPhysicist = array_pop($array);
+    return implode(", ", $array) . ", and " . $lastPhysicist . ".";
+}
+
+echo "Some of the most famous fictional theoretical physicists are " . humanizedList($physicistsArray) . PHP_EOL;
