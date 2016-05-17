e15a9ab6131c0075f5e4cb89aab0c44f8f952315
Alan <alaurit@gmail.com>
2016-05-17 09:49:56 -0500

    created square1 and square2 in test file, corrected code in Square.php

--- Square.php
+++ Square.php
@@ -4,8 +4,11 @@ require_once 'Rectangle.php';
 class Square extends Rectangle
 {
 
-    public function perimeter() {
-        return ($this->height)*2 + ($this->width)*2;       
+    public function squarePerimeter() {
+        if ($this->height != $this->width) {
+            return "This is not a square!";
+        } else
+        return ($this->height)*4; 
     }
 
 }
--- shapes_test.php
+++ shapes_test.php
@@ -1,5 +1,6 @@
 <?php
-require_once ('rectangle.php');
+require_once ('Rectangle.php');
+require_once ('Square.php');
 
 $rectangle1=new Rectangle(20, 10);
 echo $rectangle1->area() . PHP_EOL;
@@ -7,6 +8,10 @@ echo $rectangle1->area() . PHP_EOL;
 $rectangle2=new Rectangle(15, 10);
 echo $rectangle2->area() . PHP_EOL;
 
+$square1=new Square(20, 30);
+echo $square1->squarePerimeter() . PHP_EOL;
 
+$square2=new Square(20, 20);
+echo $square2->squarePerimeter() . PHP_EOL;
 
 ?>
