0e8f563074d85a314ba7397c2269d7cfcde12fb3
Alan <alaurit@gmail.com>
2016-05-17 09:32:31 -0500

    initial framework with first examples

---
+++ Rectangle.php
@@ -0,0 +1,17 @@
+<?php
+class Rectangle
+{
+    public $height;
+    public $width;
+
+    public function area() {
+        return $this->height*$this->width;
+    }
+
+    public function __construct($height, $width) {
+        $this->height=$height;
+        $this->width=$width;
+    }
+}
+
+?>
---
+++ shapes_test.php
@@ -0,0 +1,12 @@
+<?php
+require_once ('rectangle.php');
+
+$rectangle1=new Rectangle(20, 10);
+echo $rectangle1->area() . PHP_EOL;
+
+$rectangle2=new Rectangle(15, 10);
+echo $rectangle2->area() . PHP_EOL;
+
+
+
+?>
